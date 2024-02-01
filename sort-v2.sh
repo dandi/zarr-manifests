f8152d5a537479f9ef430424aead572198c43fb4
Yaroslav Halchenko <debian@onerussian.com>
2024-02-01 13:29:16 -0500

    make it possible to pass folder

--- sort-v2.sh
+++ sort-v2.sh
@@ -5,6 +5,15 @@
 topdir="$1"
 shift
 for m in "$@"; do
+	if [ -d "$m" ]; then
+		# if directory -- go through the json files there
+		# It is a trick for whenever we cannot pass too many files due
+		# hitting max cmdline size
+		for f in "$m"/*.json; do
+			"$0" "$topdir" $f
+		done
+		continue
+	fi
 	checksum=$(jq -r .statistics.zarrChecksum "$m" | sed -e 's, .*,,g')
 	ext="${m#*.}"
 	stem=$(basename $m | sed -e 's,\..*,,g')
