10be0635e030cbcc549d72c34304761bcbbfc11c
Yaroslav Halchenko <debian@onerussian.com>
2024-02-06 16:07:43 -0500

    do not remove manifests -- redo

--- get_them_all.sh
+++ get_them_all.sh
@@ -5,7 +5,7 @@ set -eu
 set -x
 
 rm -rf  times manifests*
-mkdir -p times manifests manifests-v2
+#mkdir -p times manifests manifests-v2
 
 # needs config
 #s3cmd -c ~/.s3cfg-dandi-backup ls s3://dandiarchive/zarr/ \
@@ -17,6 +17,9 @@ p=s3://dandiarchive/zarr
 aws --no-sign-request s3 ls $p/ \
 	| while read _ zarr; do
 	z=${zarr%*/}
-	/usr/bin/time -o times/$z.out ./list_bucket_prefix_versionids.py $p/$zarr > manifests/$z.json 
-	./convert_schema_1to2.py manifests/$z.json manifests-v2/$z.json &
+	/usr/bin/time -o times/$z.out ./list_bucket_prefix_versionids.py $p/$zarr >| manifests/$z.json 
+	{ 
+		./convert_schema_1to2.py manifests/$z.json manifests-v2/$z.json;
+		./sort-v2.sh zarr-manifests-v2-sorted  manifests-v2/$z.json;
+	} &
 done
