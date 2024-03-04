e80b9675b48ea1f98f007072ab588698bc6aed5e
Yaroslav Halchenko <debian@onerussian.com>
2024-03-04 18:26:10 -0500

    Also add STATS.md which points to zarrs with multiple versions for current convenience

---
+++ make_stats.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#
+set -eu
+
+cd "$(dirname "$0")"
+
+echo "## Zarrs with multiple versions"
+echo ""
+
+for zf in zarr-manifests-v2-sorted/*/*/*; do 
+	versions=( $(/bin/ls -1 "$zf"/*.json) ); 
+	if [ ${#versions[@]} != 1 ]; then
+	       z=${zf##*/}; 
+	       echo "- [$z]($zf): ${#versions[@]} versions"
+	       for v in "${versions[@]}"; do
+		       lm=$(jq -r .statistics.lastModified $v)
+		       echo "    - $lm [$(basename $v)]($v)"
+	       done | sort
+	fi
+done
