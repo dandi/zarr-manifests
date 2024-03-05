1b424ed0ddd0ddc698ef67ba7ffe2498f9f48cef
Yaroslav Halchenko <debian@onerussian.com>
2024-03-04 19:21:13 -0500

    We need to point to datasets.datalad.org since that is where we store them

--- make_stats.sh
+++ make_stats.sh
@@ -15,7 +15,7 @@ for zf in zarr-manifests-v2-sorted/*/*/*; do
 	       echo "- [$z]($zf): ${#versions[@]} versions"
 	       for v in "${versions[@]}"; do
 		       lm=$(jq -r .statistics.lastModified $v)
-		       echo "    - $lm [$(basename $v)]($v)"
+		       echo "    - $lm [$(basename $v)](https://datasets.datalad.org/dandi/zarr-manifests/$v)"
 	       done | sort
 	fi
 done
