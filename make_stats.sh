0d8a2d62788ac5f1c4367eeb17cfa79122bb7c47
Yaroslav Halchenko <debian@onerussian.com>
2024-03-04 19:46:11 -0500

    detabify the script and make more handy urls to explore those zarrs

--- make_stats.sh
+++ make_stats.sh
@@ -8,14 +8,15 @@ cd "$(dirname "$0")"
 echo "## Zarrs with multiple versions"
 echo ""
 
-for zf in zarr-manifests-v2-sorted/*/*/*; do 
-	versions=( $(/bin/ls -1 "$zf"/*.json) ); 
-	if [ ${#versions[@]} != 1 ]; then
-	       z=${zf##*/}; 
-	       echo "- [$z]($zf): ${#versions[@]} versions"
-	       for v in "${versions[@]}"; do
-		       lm=$(jq -r .statistics.lastModified $v)
-		       echo "    - $lm [$(basename $v)](https://datasets.datalad.org/dandi/zarr-manifests/$v)"
-	       done | sort
-	fi
+for zf in zarr-manifests-v2-sorted/*/*/*; do
+    versions=( $(/bin/ls -1 "$zf"/*.json) );
+    if [ ${#versions[@]} != 1 ]; then
+        z=${zf##*/};
+        zfp=${zf#*/};
+        echo "- [$z]($zf): ${#versions[@]} versions [dandisets manifests](https://datasets.datalad.org/dandi/zarr-manifests/$zf) [dandidav/zarrs](https://dandi.centerforopenneuroscience.org/zarrs/$zfp)"
+        for v in "${versions[@]}"; do
+            lm=$(jq -r .statistics.lastModified $v)
+            echo "    - $lm [$(basename $v)](https://datasets.datalad.org/dandi/zarr-manifests/$v)"
+        done | sort
+    fi
 done
