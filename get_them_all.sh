9d860c17add3f16c5aebfa4947026ffc412bdbf3
Yaroslav Halchenko <debian@onerussian.com>
2024-03-18 16:14:42 -0400

    Cron job would now rely on update_manifest to do looping

--- get_them_all.sh
+++ get_them_all.sh
@@ -4,10 +4,12 @@ set -eu -o pipefail
 set -x
 
 p=s3://dandiarchive/zarr
-aws --no-sign-request s3 ls $p/ \
-    | sed -ne '/.*-.*-.*/s,.*PRE \(.*\)/,\1,gp' \
-    | parallel -j 10 ./update_manifest.py -i dandi --mode api-check --manifests-root zarr-manifests-v2-sorted '{}'
 
+# IF we are to base of what we find on S3
+#aws --no-sign-request s3 ls $p/ \
+#    | sed -ne '/.*-.*-.*/s,.*PRE \(.*\)/,\1,gp' \
+#    | parallel -j 10 ./update_manifest.py -i dandi --mode api-check --manifests-root zarr-manifests-v2-sorted '{}'
+./update_manifest.py -i dandi --mode api-check --manifests-root zarr-manifests-v2-sorted
 rm -f STATS.md
 ./make_stats.sh  >| STATS.md
 
