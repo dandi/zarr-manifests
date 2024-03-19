0ee4336f94196b85c6e4d8127cf0c06122ea08bb
Yaroslav Halchenko <debian@onerussian.com>
2024-03-19 08:40:58 -0400

    Do pass args if given in get_them_all

--- get_them_all.sh
+++ get_them_all.sh
@@ -9,7 +9,7 @@ p=s3://dandiarchive/zarr
 #aws --no-sign-request s3 ls $p/ \
 #    | sed -ne '/.*-.*-.*/s,.*PRE \(.*\)/,\1,gp' \
 #    | parallel -j 10 ./update_manifest.py -i dandi --mode api-check --manifests-root zarr-manifests-v2-sorted '{}'
-./update_manifest.py -i dandi --mode api-check --manifests-root zarr-manifests-v2-sorted
+./update_manifest.py -i dandi --mode api-check --manifests-root zarr-manifests-v2-sorted "$@"
 rm -f STATS.md
 ./make_stats.sh  >| STATS.md
 
